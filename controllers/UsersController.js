fca4150884b18d97610054fc57a46ba834037069
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-08-05 11:32:57 -0500

    Created more APIs

---
+++ controllers/UsersController.js
@@ -0,0 +1,58 @@
+// UsersController.js
+
+const crypto = require('crypto');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const result = await dbClient.db.collection('users').insertOne(newUser);
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.users.findOne({ _id: dbClient.ObjectID(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = UsersController;
