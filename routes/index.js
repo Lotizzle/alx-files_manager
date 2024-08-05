36d323d92192fa28aa1694f0f46f75b415902b3b
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-08-05 11:49:40 -0500

    Created more APIs

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-// index.js
+// routes/index.js
 
 const express = require('express');
 const AppController = require('../controllers/AppController');
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
 
 module.exports = router;
